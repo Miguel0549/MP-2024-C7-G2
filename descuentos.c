878fa86e0a8ca7f4fa5cd1d46a5e16492b5cb71f
Manueh404 <ma.diazdelarochadecas@alum.uca.es>
2024-03-12 19:49:54 +0100

    Tipo descuentos creado y función crear_fichero_descuentos incluida

--- descuentos.c
+++ descuentos.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include "descuentos.h"
+void crear_fichero_descuentos();
+
 int main(){   //main para pruebas, quitar a la hora de unir los módulos
+crear_fichero_descuentos;
+return 0;
+}
 
 
-
-
+//cabecera: crear_fichero_descuentos()
+//preconción: ninguna
+//postconción: crea el fichero descuentos
+void crear_fichero_descuentos()
+{
+    FILE* f;
+    if ((f = fopen("../Descuentos/descuentos.txt", "w")) == NULL)
+        printf("Error al abrir el archivo\n");
+    fclose(f);
 }
--- descuentos.h
+++ descuentos.h
@@ -1,5 +1,18 @@
 /* Fichero  en que irán las funciones del módulo descuentos que se necesiten en otros modulos*/
+typedef enum { 
+    codpro, cheqreg, // tipos de descuentos
+    activo, inactivo, //estados de los descuentos 
+    todos, esizon //Aplicabilidad de los descuentos en los productos
+}opciones;
 
-//cabecera:
-//precondición:
-//postcondiciión:
+/*TIPO DESCUENTOS*/
+typedef struct descuentos
+{
+    char Id_cod[10],Descrip[50];
+    int Importe;    
+    opciones Tipo, Estado, Aplicabilidad;
+}Descuentos;
+
+//cabecera: 
+//precondición: 
+//postcondición:  
