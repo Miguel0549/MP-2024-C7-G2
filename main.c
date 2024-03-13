c35246366aa9e34940cb9d912a3c6d2c8d0339eb
Manueh404 <ma.diazdelarochadecas@alum.uca.es>
2024-03-13 19:15:51 +0100

    Cabeceras de las funciones de menu de cada tipo de sesión

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-
+//#include <>  añadir los fuicheros necesarios 
 int main(){
     printf("texto de prueba\n");
 
 
     return 0;
 }
+
+//cabecera: void menu_principal_usuario(char Id_usuario[7])
+//precoindición: recibe el id del usuario que ha iniciado sesión 
+//postcondición: imprime el menú de usuario
+
+//cabecera: void menu_principal_transportista(char transportista[7])
+//precoindición: recibe el id del transportista que ha iniciado sesión 
+//postcondición: imprime el menú de transportista
+
+//cabecera: void menu_principal_proveedor(char Id_proveedor[7])
+//precoindición: recibe el id del usuario que ha iniciado sesión 
+//postcondición: imprime el menú de proveedor
+
+//cabecera: void menu_principal_administrador(char Id_admin[7])
+//precoindición: recibe el id del usuario que ha iniciado sesión 
+//postcondición: imprime el menú de administrador 
