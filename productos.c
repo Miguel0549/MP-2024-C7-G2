af82c227710e4fe1da8e587c47cf2dbe43ccde5c
Pablo Beato <paul.999@hotmail.es>
2024-03-17 19:10:24 +0100

    Desarrollo del dia

--- productos.c
+++ productos.c
@@ -1,16 +1,62 @@
 #include "productos.h"
 #include <string.h>
 #include <stdio.h>
-void alta_producto()//Por hacer
+#include <stdlib.h>
+void guardar_producto(producto *v)
 {
     FILE *f;
-    char nulo='\n';
-    int id;
-    if ((f=fopen(F_PRODUCTO,"r+"))!=NULL){
-        id=buscar_id(&f,&nulo);
+    int i;
+    int n_elem=sizeof(*v)/sizeof(producto);
+    if ((f=fopen(F_PRODUCTO,"w"))==NULL)
+    {
+        printf("Error de apertura de fichero.");
+    }
+    else
+    {
+        for (i=0;i<n_elem;i++)//Volcado de un elemento
+        {
+            fputs(v[i].id_prod,f);
+            fputc('-',f);
+            fputs(v[i].nombre,f);
+            fputc('-',f);
+            fputs(v[i].descrip,f);
+            fputc('-',f);
+            fputs(v[i].id_categ,f);
+            fputc('-',f);
+            fputs(v[i].id_gestor,f);
+            fputc('-',f);
+            fprintf(f,"%d",v[i].stock);
+            fputc('-',f);
+            fprintf(f,"%d",v[i].stock);
+            fputc('-',f);
+            fprintf(f,"%d",v[i].entrega);
+            fputc('-',f);
+            fprintf(f,"%d",v[i].importe);
+            fputc('\n',f);
+        }
     }
 }
-int buscar_id(FILE **f,char *n)//Posible error, necesita ser probado
+void guardar_categoria(categoria *v)
+{
+    FILE *f;
+    int i;
+    int n_elem=sizeof(*v)/sizeof(categoria);
+    if ((f=fopen(F_CATEGORIAS,"w"))==NULL)
+    {
+        printf("Error de apertura de fichero.");
+    }
+    else
+    {
+        for (i=0;i<n_elem;i++)//Volcado de un elemento
+        {
+            fputs(v[i].id_cat,f);
+            fputc('-',f);
+            fputs(v[i].descrip,f);
+            fputc('-',f);
+        }
+    }
+}
+int buscar_id_f(FILE **f,char *n)
 {
     char d[51];
     char i;
@@ -30,8 +76,12 @@ int buscar_id(FILE **f,char *n)//Posible error, necesita ser probado
             d[j]=i;
         }
         d[j-1]='\0';
+        //Mueve el cursor hasta la siguiente id
+        while(i!=EOF&&i!='\n')
+        {
+            i=fgetc(*f);
+        }
 
     }while(strcmp(n,d)!=0);//comparacion de cadena actual con la cadena introducida
-    fclose(*f);
     return id;
 }
--- productos.h
+++ productos.h
@@ -4,6 +4,7 @@
 #define F_CATEGORIAS "Categorias.txt"
 #include <stdio.h>
 typedef struct {
+    char nombre[16];
     char descrip[51];
     char id_prod[8];
     char id_gestor[5];
@@ -14,7 +15,22 @@ typedef struct {
     char id_cat[5];
     char descrip[51];
 }categoria;
-
+//Cabecera: void volcar_producto(producto *v)
+//Precondicion: v debe de ser una cadena de tipo producto definida de forma dinámica
+//poscondicion: Vuelca todos la informacion de los productos en el fichero
+void volcar_producto(producto *v);
+//Cabecera: void guardar_producto(producto *v)
+//Precondicion: v debe de ser una cadena de tipo producto definida de forma dinámica
+//Poscondición: Guarda todos los miembros de producto en el fichero "Productos.txt"
+void guardar_producto(producto *v);
+//Cabecera: void guardar_categoria(producto *v)
+//Precondicion: v debe de ser una cadena de tipo categoria definida de forma dinámica
+//Poscondición: Guarda todos los miembros de categoria en el fichero "Productos.txt"
+void guardar_categoria(categoria *v);
+//Cabecera: void volcar_categoria(categoria *v)
+//Precondicion: v debe de ser una cadena de tipo categoria definida de forma dinamica
+//Poscondicion: Vuelca toda la informacion de las categorias en el fichero
+void volcar_categoria(categoria *v);
 //Cabecera: void alta_producto(producto p)
 //Precondicion: usuario debe estar definido 
 //Poscondicion: Lee toda los campos de producto por teclado excepto por producto.id_gestor y
@@ -24,7 +40,6 @@ void alta_producto(/*usuario*/);
 //Precondicion: id debe pertenecer a un producto
 //Poscondicion: Borra la informacion de un producto
 void baja_producto(char *id);
-//Cabecera: void alta_categoria
 //Cabecera: void mod_prod(producto)
 //Precondicion: producto.id_prod=id del producto a modificar
 //Poscondicion: Cambia los datos del producto con id producto.id_prod
@@ -32,7 +47,7 @@ void mod_prod(producto);
 //Cabecera: int buscar_id (FILE *f,char *n)
 //Precondicion: f debe ser F_PRODUCTO o F_CATEGORIAS y estar abierto para lectura
 //poscondicion: Devuelve la id de n dentro del fichero
-int buscar_id(FILE **f,char *n);
+int buscar_id_f(FILE **f,char *n);
 //Cabecera: void Busqueda_prod_nombre(char *nombre)
 //Precondicion: nombre debe seruna cadena ya definida
 //Poscondicion: Escribe por pantalla todos los productos cuya descripcion coincida con nombre
