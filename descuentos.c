7c60a34ea502da54885a815100f7d5eb6d3ba8ec
Manueh404 <ma.diazdelarochadecas@alum.uca.es>
2024-03-22 17:40:04 +0100

    función nuevo descuento includa, también leer string

--- descuentos.c
+++ descuentos.c
@@ -6,22 +6,24 @@
 #include "descuentos.h"
 #define LINE 100
 
-Descuentos convertir_cadena_a_Descuentos(char*);
 //Tipos 
 typedef struct fecha{
     int dia, mes, anno;
 }fecha;
 void crear_fichero_descuentos();
 void crear_fichero_descuentos_clientes();
+void leer_string(char*,int);
+void nuevo_descuento(Descuentos**,int);
 
 int main(){   //main para pruebas, quitar a la hora de unir los módulos
 Descuentos *d;
-int n=0,*p;
+int n=1;
+int *p;
 p = &n;
 crear_fichero_descuentos();
 //crear_fichero_descuentos_clientes;
-carga_descuentos(&d,p);
-
+//carga_descuentos(&d,p);
+nuevo_descuento(&d,n);
 printf("Id: %s\n",d[0].Id_cod);
 return 0;
 }
@@ -50,8 +52,44 @@ void crear_fichero_descuentos_clientes()
 
 
 
+//cabecera: void nuevo_descuento(int *n_desc)
+//precondición: *n_desc puntero que apunta al número total de descuentos actual
+//postcondición: crea y añade a la estructura un nuevo descuento
+void nuevo_descuento(Descuentos **d,int n_desc){
+    int sel,i=0;
+    (*d )= (Descuentos *)calloc(n_desc, sizeof(Descuentos));
+    if(d==NULL){
+        printf("Error al reservar memoria para los Descuentos\n");
+        exit(1);
+    }
+
+    printf("\nIntrozuca la Id del descuento:\n");     
+    leer_string((*d)[n_desc-1].Id_cod,11);           //Introducimos la Id del código
+    
+    printf("\nIntroduzca una breve descripción del descuento:\n");
+    leer_string((*d)[n_desc-1].Descrip,51);                            //Introducimos la cadena
+
+    printf("\nIntroduce el importe del descuento:\n");
+    leer_string((*d)[n_desc-1].Importe,4);               //Introducimos el importe del descuento
+    do{
+        printf("\nElija un tipo de descuento:\n 1.Codpro\n 2.Cheqreg\n");
+        scanf("%i",&sel);
+        if (sel==1)
+        {
+            (*d)[n_desc-1].Tipo=codpro;
+            i++;
+        }else if (sel==2)
+        {
+            (*d)[n_desc-1].Tipo=cheqreg;
+            i++;
+        }else
+        {
+            printf("\nIntroduzca una opción válida\n");
+        }
+    }while(i==0);
 
 
+}
 
 
 // cabecera: fecha fecha_actual()
@@ -71,8 +109,28 @@ fecha fecha_actual()
     return fecha_actual;
 }
 
+// cabecera: void leer_string(char * cadena, int elem)
+// precondicion: ninguna
+// postcondicion: lee en cadena la cadena introducida por el usuario
+void leer_string(char*cadena, int elem)
+{
+    //int i;
 
+    fflush(stdin);
+    fgets(cadena, elem , stdin);
+    fflush(stdin);
+    puts(cadena);
+    /*for (i = 0; cadena[i] != '\n' && i < elem + 1; i++)
+        if (cadena[i] == '\n')
+            cadena[i] = '\0';
 
+    i = 0;
+    while (i < elem + 1) {
+        if (cadena[i] == '\n')
+            cadena[i] = '\0';
+        i++;
+    }*/
+}
 // cabecera: int comparar_fechas(fecha fecha1, fecha fecha2)
 // precondicion: fecha1 y fecha2 inicializados
 // postcondicion: devuelve => =0 - fechas iguales
@@ -105,6 +163,9 @@ int comparar_fechas(fecha fecha1, fecha fecha2)
 
 
 
+//cabecera: void carga_descuentos(Descuentos **d,int *n_desc)
+//precondición: sea **d un doble puntero a una estructura descuentos y *n_desc un puntero a entero con el número total de descuentos
+//postcondición: carga en el vector de estructura Descuentos los datos desde el fichero descuentos.txt
 void carga_descuentos(Descuentos **d,int *n_desc){
     int i;
     char line[LINE]="\0";
@@ -174,6 +235,11 @@ void carga_descuentos(Descuentos **d,int *n_desc){
 
 }
 
+
+
+//cabecera:void volcado_descuentos(Descuentos **d,int *n_desc)
+//precondición:**d incializado y descuentos.txt creado
+//postcondición:descuentos.txt contiene la información que contenía el vector de estructuras
 
 /*
 void carga_descuentos_clientes(Descuentos_clientes **dc){
