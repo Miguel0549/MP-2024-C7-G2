53114ba04f1b48fca947b8c34d317d2cb671ae92
Manueh404 <ma.diazdelarochadecas@alum.uca.es>
2024-04-06 13:32:46 +0200

    volcado y carga de transportistas y adminprov

--- sesiones.c
+++ sesiones.c
@@ -11,6 +11,8 @@
 
 char* convertir_entero_char(int);
 int num_cliente();
+int num_adminprov();
+int num_transp();
 
 
 int main(){ // main para pruebas
@@ -33,9 +35,6 @@ void crear_fichero_clientes()
     fclose(f2);
 }
 
-//cabecera: void carga_descuentos(Clientes **d)
-//precondición: sea **c un doble puntero a una estructura clientes
-//postcondición: carga en el vector de estructura Descuentos los datos desde el fichero clientes.txt
 void carga_cliente(Cliente **c){
     int i,n_clien;
     char line[LINE]="\0";
@@ -69,9 +68,6 @@ void carga_cliente(Cliente **c){
         fclose(f);
 }
 
-//cabecera:void volcado_descuentos(Clientes **c)
-//precondición:**c incializado y descuentos.txt creado
-//postcondición:descuentos.txt contiene la información que contenía el vector de estructuras
 void volcado_cliente(Cliente **c){
 
     int i,n_clien;
@@ -115,7 +111,6 @@ void volcado_cliente(Cliente **c){
 
 }
 
-
 //cabecera: int num_cliente()
 //precondición clientes.txt existente
 //postcondicióin: devuleve el número de líneas que hay escritas en el fichero
@@ -143,5 +138,204 @@ int num_cliente(){
             }
         }
  return (n_lin+1);
+
+}
+
+//cabecera: int num_adminprov()
+//precondición adminprov.txt existente
+//postcondicióin: devuleve el número de líneas que hay escritas en el fichero
+int num_adminprov(){
+    char c; 
+    int n_lin,i;
+    n_lin=0;
+    i=0;
+
+    FILE *f;
+    if((f= fopen("adminprov.txt","r"))==NULL){
+        printf("Error al abrir el archivo\n");
+        }else{
+            while(i==0){
+
+                c = fgetc(f);
+
+                if(c == '\n'){
+                    n_lin++;
+                }
+                if(c == EOF){  //Si el caracter es end of file imprimimos el contador y salimos del while
+                // printf("%i",n_lin);  //El número de lineas
+                    i++;
+                }
+            }
+        }
+ return (n_lin+1);
+
+}
+
+//cabecera: int num_transp()
+//precondición transportistas.txt existente
+//postcondicióin: devuleve el número de líneas que hay escritas en el fichero
+int num_transp(){
+    char c; 
+    int n_lin,i;
+    n_lin=0;
+    i=0;
+
+    FILE *f;
+    if((f= fopen("transportistas.txt","r"))==NULL){
+        printf("Error al abrir el archivo\n");
+        }else{
+            while(i==0){
+
+                c = fgetc(f);
+
+                if(c == '\n'){
+                    n_lin++;
+                }
+                if(c == EOF){  //Si el caracter es end of file imprimimos el contador y salimos del while
+                // printf("%i",n_lin);  //El número de lineas
+                    i++;
+                }
+            }
+        }
+ return (n_lin+1);
+
+}
+
+void carga_adminprov(Adminprov **a){
+    int i,n_admin;
+    char line[LINE]="\0";
+    char estado[10]="\0";
+    FILE *f;
+    n_admin=num_adminprov();
+
+    if((f=fopen("adminprov.txt","r"))==NULL)
+        printf("Error al abrir el archivo\n");
+    
+        *a = (Adminprov *)calloc(n_admin, sizeof(Adminprov));
+        if(*a==NULL){
+            printf("Error al reservar memoria para los Descuentos\n");
+            exit(1);
+        }else{
+            rewind(f);
+            for(i=0;i<n_admin;i++){
+
+                fgets(line,LINE,f);
+                strcpy((*a)[i].Id_empresa, strtok(line,"-"));
+                strcpy((*a)[i].Nombre, strtok(NULL, "-\n"));  //cargamos los datos tipo char
+                strcpy((*a)[i].email, strtok(NULL, "-\n"));
+                strcpy((*a)[i].Contrasenna, strtok(NULL, "-\n"));
+
+                strcpy(estado, strtok(NULL, "-\n"));//introducimos la siguiente cadena entre los '-'
+                    
+
+                 if(strcmp(estado,"administrador")==0){
+                        (*a)[i].Perfil_usuario=administrador;
+
+                 }else if(strcmp(estado, "proveedor")==0){
+                        (*a)[i].Perfil_usuario=proveedor;
+                }else{puts("Error del campo TIPO PROV/ADMIN"); }
+
+                for(int k=0;k<10;k++) estado[k]='\0';
+            
+            }   
+        }
+        rewind(f);
+        fclose(f);
+}
+
+void volcado_adminprov(Adminprov **a){
+    int i,n_admin;
+    char line[LINE]="\0";
+    FILE *f;
+    n_admin=num_adminprov();
+
+    if ((f = fopen("adminprov.txt", "w+")) == NULL) {
+
+        printf("Error al abrir el archivo");
+
+    } else {
+
+        for ( i=0 ; i<n_admin ; i++ ){
+           // strcpy(line,"\n");
+            strcpy(line, (*a)[i].Id_empresa);
+            strcat(line, "-");
+            strcat(line, (*a)[i].Nombre);
+            strcat(line, "-");
+            strcat(line, (*a)[i].email);
+            strcat(line, "-");
+            strcat(line, (*a)[i].Contrasenna);
+            strcat(line, "-");
+            
+           if(i < n_admin - 1) strcat(line, "\n");
+            fprintf(f, "%s", line);
+        }
+
+    }
+
+    fclose(f);
+
+}
+
+void carga_transp(Transportista**t){
+    int i,n_trans;
+    char line[LINE]="\0";
+    FILE *f;
+    n_trans=num_transp();
+
+    if((f=fopen("transportistas.txt","r"))==NULL)
+        printf("Error al abrir el archivo\n");
+    
+        *t = (Transportista *)calloc(n_trans, sizeof(Transportista));
+        if(*t==NULL){
+            printf("Error al reservar memoria para los Descuentos\n");
+            exit(1);
+        }else{
+            rewind(f);
+            for(i=0;i<n_trans;i++){
+
+                fgets(line,LINE,f);
+                strcpy((*t)[i].Id_transp, strtok(line,"-"));
+                strcpy((*t)[i].Nombre, strtok(NULL, "-\n"));  //cargamos los datos tipo char
+                strcpy((*t)[i].email, strtok(NULL, "-\n"));
+                strcpy((*t)[i].Contrasenna, strtok(NULL, "-\n"));
+                strcpy((*t)[i].Nom_empre, strtok(NULL, "-\n"));
+                strcpy((*t)[i].Ciudad, strtok(NULL, "-\n"));
+            
+            }   
+        }
+        rewind(f);
+        fclose(f);
+}
+
+void volcado_transp(Transportista **t){
+    int i,n_trans;
+    char line[LINE]="\0";
+    FILE *f;
+    n_trans=num_transp();
+
+    if ((f = fopen("transportistas.txt", "w+")) == NULL) {
+
+        printf("Error al abrir el archivo");
+
+    } else {
+
+        for ( i=0 ; i<n_trans ; i++ ){
+           // strcpy(line,"\n");
+            strcpy(line, (*t)[i].Id_transp);
+            strcat(line, "-");
+            strcat(line, (*t)[i].Nombre);
+            strcat(line, "-");
+            strcat(line, (*t)[i].email);
+            strcat(line, "-");
+            strcat(line, (*t)[i].Contrasenna);
+            strcat(line, "-");
+            
+           if(i < n_trans - 1) strcat(line, "\n");
+            fprintf(f, "%s", line);
+        }
+
+    }
+
+    fclose(f);
 
 }
--- sesiones.h
+++ sesiones.h
@@ -18,5 +18,33 @@ typedef struct {
     char Id_transp[4],Nombre[20],email[30],Contrasenna[15],Nom_empre[20],Ciudad[20];
 }Transportista;
 
+
+//cabecera void carga_cliente(Cliente**)
+//precondición:fichero inicializado
+//postcondición:carga en el doble puntero a estructura cliente la información del fichero
 void carga_cliente(Cliente**);
+
+//cabecera void volcado_cliente(Cliente**)
+//precondición:fichero y estructura inicializados
+//postcondición:vuelca la información al fichero
 void volcado_cliente(Cliente **);
+
+//cabecera void carga_Adminprov(Adminprov**)
+//precondición:fichero inicializado
+//postcondición:carga en el doble puntero a estructura adminprov la información del fichero
+void carga_adminprov(Adminprov**);
+
+//cabecera void volcado_adminprov(Adminprov**)
+//precondición:fichero y vector de estrtuctuas inicializados
+//postcondición:vuelca al fiichero la información del vector de estructuras
+void volcado_adminprov(Adminprov**);
+
+//cabecera void carga_transp(Transportista**)
+//precondición:fichero inicializado
+//postcondición:carga en el doble puntero a estructura Transportista la información del fichero
+void carga_transp(Transportista**);
+
+//cabecera void volcado_transp(Transportista**)
+//precondición:fichero inicializado, así como el vector de estructura transportista
+//postcondición:vuelca la información del vector de estructura Transportista al fichero
+void volcado_transp(Transportista**);
